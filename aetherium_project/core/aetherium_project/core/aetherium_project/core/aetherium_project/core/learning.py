819dc95e4bec35d15a0cd82b91c2f75c2cfa299b
Justkruz <justkruz12@gmail.com>
2024-12-12 12:13:22 -0500

    Create learning.py
    Create learning.PY file

---
+++ aetherium_project/core/aetherium_project/core/aetherium_project/core/aetherium_project/core/learning.py
@@ -0,0 +1,7 @@
+# aetherium_project/core/learning.py
+# Placeholder for learning mechanisms
+class LearningMechanism:
+    def __init__(self):
+        pass
+    def learn(self, agent, environment):
+        pass
